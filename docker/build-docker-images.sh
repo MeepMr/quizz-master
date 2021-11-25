ac2c319ce3674a6f5e49261a8e8875d695169228
justinklein <jus.klein@icloud.com>
2021-11-25 19:31:36 +0100

    feat(docker): reworked the docker-scripts

---
+++ docker/build-docker-images.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build ./../quizz-master-frontend/ --file ./qm-frontend/Dockerfile -t quizz-master-frontend
--- docker/qm-frontend/build-docker-frontend.sh
+++
@@ -1,1 +0,0 @@
-docker buildx build --platform linux/arm,linux/arm64,linux/amd64 ./../../quizz-master-frontend/ --file ./Dockerfile -t mrmeep/quizz-master-frontend --push
