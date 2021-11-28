506eb5aefda6dc433f144bf1df06d4b69f116d3f
justinklein <jus.klein@icloud.com>
2021-11-28 22:14:33 +0100

    feat(question): clicked questions will be marked as answered

--- quizz-master-frontend/src/app/components/quiz-question/quiz-question.component.ts
+++ quizz-master-frontend/src/app/components/quiz-question/quiz-question.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {QuizQuestion} from "../../shared/model/quiz/quiz-question";
 
 @Component({
@@ -6,11 +6,17 @@ import {QuizQuestion} from "../../shared/model/quiz/quiz-question";
   templateUrl: './quiz-question.component.html',
   styleUrls: ['./quiz-question.component.scss']
 })
-export class QuizQuestionComponent implements OnInit {
+export class QuizQuestionComponent implements OnInit, OnDestroy {
 
-  @Input() activeQuestion?: QuizQuestion | null;
+  @Input() activeQuestion?: QuizQuestion | null = new QuizQuestion(-1, '', -1);
 
   ngOnInit(): void {
     const uselessBoolean: boolean = true;
+  }
+
+  ngOnDestroy(): void {
+
+    if (this.activeQuestion)
+      this.activeQuestion.answered = true;
   }
 }
--- quizz-master-frontend/src/app/services/question-service/question.service.ts
+++ quizz-master-frontend/src/app/services/question-service/question.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 import {QUESTION_MOCK} from "../../shared/mock/question-mock";
 import {Observable, of} from "rxjs";
 import {QuizQuestion} from "../../shared/model/quiz/quiz-question";
+import {QuizService} from "../quiz-service/quiz.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionService {
 
-  questionMock: QUESTION_MOCK = new QUESTION_MOCK(0);
+  constructor(private quizService: QuizService) { }
+
+  questionMock: QUESTION_MOCK = this.quizService.quizMock.questionMock;
 
   getQuestion$(id: number): Observable<QuizQuestion> {
     return of(this.questionMock.getMockData()[id]);
