340e0f059891a3abb1d98f21903b7ceed71b2693
justinklein <jus.klein@icloud.com>
2021-12-01 21:42:24 +0100

    refactor(backend): fixed linting issues

--- quizz-master-backend/qm-backend-app/src/main/java/de/jklein/qmbackendapp/TestController.java
+++ quizz-master-backend/qm-backend-app/src/main/java/de/jklein/qmbackendapp/TestController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/")
-    public String helloWorld () {
-        return "Hello World";
-    }
+  @GetMapping("/")
+  public String helloWorld() {
+    return "Hello World";
+  }
 }
