6b7fd22676ee41bcd7c51154d4a6047bc9700658
justinklein <jus.klein@icloud.com>
2021-12-03 23:17:40 +0100

    feat(yes-no-question): implemented

--- quizz-master-frontend/src/app/components/quiz-question/show-yes-no-question/show-yes-no-question.component.ts
+++ quizz-master-frontend/src/app/components/quiz-question/show-yes-no-question/show-yes-no-question.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {YesNoQuestion} from "../../../shared/model/question/yes-no-question";
 
 @Component({
@@ -6,13 +6,23 @@ import {YesNoQuestion} from "../../../shared/model/question/yes-no-question";
   templateUrl: './show-yes-no-question.component.html',
   styleUrls: ['./show-yes-no-question.component.scss']
 })
-export class ShowYesNoQuestionComponent implements OnInit {
+export class ShowYesNoQuestionComponent {
 
-  @Input() activeYesNoQuestion?: YesNoQuestion;
+  @Input() activeYesNoQuestion: YesNoQuestion = new YesNoQuestion(-1, '', -1);
 
-  constructor() { }
+  answered: boolean = false;
+  answeredCorrectly: boolean = false;
+  answer?: boolean;
 
-  ngOnInit(): void {
+  private answeredQuestion(): void {
+
+    this.answered = true;
+    this.activeYesNoQuestion.answered = true;
   }
 
+  answerQuestion(answer: boolean): void {
+    this.answeredQuestion();
+    this.answer = answer;
+    this.answeredCorrectly = !answer == this.activeYesNoQuestion.correct;
+  }
 }
--- quizz-master-frontend/src/app/shared/model/question/yes-no-question.ts
+++ quizz-master-frontend/src/app/shared/model/question/yes-no-question.ts
@@ -3,8 +3,11 @@ import { QuestionType } from './question-type';
 
 export class YesNoQuestion extends QuizQuestion {
 
-  constructor(id: number, questionText: String, points: number) {
+  correct: boolean;
+
+  constructor(id: number, questionText: String, points: number, correct: boolean = true) {
     super(id, questionText, points);
     this.questionType = QuestionType.YES_NO_QUESTION;
+    this.correct = correct;
   }
 }
